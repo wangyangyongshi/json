665ffc36bf16febadb2b9c2192bee1d9712f05eb
wangyang <wangyang8015@duia.com>
2019-01-18 18:30:15 +0800

    thread

--- src/main/java/com/example/demo/controller/ThreadController.java
+++ src/main/java/com/example/demo/controller/ThreadController.java
@@ -8,6 +8,7 @@ public class ThreadController {
      * @param args jvm会等待非守护线程，但是不会等待守护线程，守护线程--》gc线程
      */
     public static void main(String[] args) {
-
+        //创建两个线程，其中一个输出1-52，另外一个输出A-Z。输出格式要求：12A 34B 56C 78D
+        //https://blog.csdn.net/u014297722/article/details/70174038
     }
 }
